e8e9a54fe34ceeb60d52a44bd26e5d1fc7bb4c2a
Rick Brown <ricky.brown.00@gmail.com>
2020-11-20 21:02:52 +0000

    Initial Commit

---
+++ Firebase Login Template/Firebase_Login_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Login_TemplateApp.swift
+//  Firebase Login Template
+//
+//  Created by Rick Brown on 20/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Login_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
